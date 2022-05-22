600f8cdeee1bec19e237453eb24bc906be7d736e
Alain Bertrand <alainbe@free.fr>
2022-05-22 18:11:08 +0200

    terminé de préparer les questions

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ document.getElementById("case_button").addEventListener("click", toggleCase);
 document.getElementById("num_button").addEventListener("click", toggleNum);
 document.getElementById("freq_button").addEventListener("click", toggleFreq);
 
-
+ /******************************************   
+  * 
+  * GUI update functions
+  * 
+  * ****************************************/
 function toggleFreq(){
     let myDiv = document.getElementById("freq");
     if (myDiv.style.display == "block"){
@@ -16,6 +20,7 @@ function toggleFreq(){
     }
 }
 function toggleNum(){
+    //show the div
     let myDiv = document.getElementById("num");
     if (myDiv.style.display == "block"){
         myDiv.style.display = "none";
@@ -24,6 +29,7 @@ function toggleNum(){
     }
 }
 function toggleCase(){
+    // show the div 
     let myDiv = document.getElementById("case");
     if (myDiv.style.display == "block"){
         myDiv.style.display = "none";
@@ -33,6 +39,7 @@ function toggleCase(){
 }
 
 function togglePos(){
+        // show the div 
     let myDiv = document.getElementById("pos");
     if (myDiv.style.display == "block"){
         myDiv.style.display = "none";
@@ -42,8 +49,31 @@ function togglePos(){
 }
 
 function chooseDecl(){
+    // init the param chekboxes with correct values
+//set the checked state at correct values 
+    document.getElementById("sub_check").checked = pos_target['sub'];
+    document.getElementById("adj_check").checked = pos_target['adj'];
+    document.getElementById("pron_check").checked = pos_target['pron'];
+    // set the checked state at correct values
+    for([key, val] of Object.entries(decl_target)) {
+        button = document.getElementById(key+"_check");
+        console.log(key+"_check");
+        button.checked = val;
+        //console.log(key, val);
+      }
+    //set the checked stat at correct values
+    for([key, val] of Object.entries(number_target)) {
+        button = document.getElementById(key+"_check");
+        button.checked = val;
+        //console.log(key, val);
+      }
+    // set the correct button checked
+    button = document.getElementById(freq_target+"_rad");
+    button.checked = true ;
+
+
     document.getElementById("decl_param").style.display="block";
-    console.log("declDiv in chooseDecl",declDivb);
+    //console.log("declDiv in chooseDecl",declDiv);
     }
 function chooseConj(){
     let declDiv = document.getElementById("decl_param");
@@ -67,10 +97,176 @@ async function getCase(db){
     return result;
 }
 
-async function startDrill(){
-    const fs = require("fs")
-    const sqlite = require("aa-sqlite")
-    await sqlite.open('cregr_db.db');
-    let caseText = document.getElementById("case");
-    caseText.innerHTML = await getCase(sqlite)
+ /******************************************   
+  * 
+  *  drill  controlling functions
+  * 
+  * ****************************************/
+function paramOK() {
+    // check pos
+    result= true;
+    posOK = false;
+    for ([key, val] of Object.entries(pos_target)) {
+        chain = key + "_check";
+        //console.log("chain = ", chain);
+        posOK = posOK || document.getElementById(key + "_check").checked;
+        console.log("posOK", posOK);
     }
+    // check cases
+    caseOK = false;
+    for ([key, val] of Object.entries(decl_target)) {
+        caseOK = caseOK || document.getElementById(key + "_check").checked;
+        console.log("caseOK", caseOK);
+    }
+    //check num
+    numOK = false;
+    
+    for ([key, val] of Object.entries(number_target)) {
+        numOK = numOK || document.getElementById(key + "_check").checked;
+        console.log("numOK", numOK);
+    }
+    if (!(posOK && caseOK && numOK)) {
+        allready = false;
+        begin = '';
+        alertStr = "Attention : il faut cocher au moins une case pour "
+        if (!posOK) {
+            alertStr = alertStr + "le type de mot ";
+            begin = ', '
+        }
+        if(! caseOK){
+            alertStr = alertStr + begin + "le cas";
+        }
+
+        if (!numOK) {
+            if (begin == ', ') {
+                begin = ' et ';
+            }
+            alertStr = alertStr + begin + "le nombre";
+        }
+    alertStr = alertStr + '.';
+    alert(alertStr);
+    }
+    return result;
+}
+
+/**
+ * 
+ * @returns les types grammaticaux choisis dans une liste
+ */
+function retrievePos(){ 
+    choosen = [] ;
+    count = 0;
+    for ([key, val] of Object.entries(pos_target)) {
+        //console.log("chain = ", chain);
+        if (document.getElementById(key + "_check").checked){
+            choosen[count] = key;
+            count++ ;
+        }
+    }
+    console.log("choosen ps", choosen);
+    return choosen
+}
+/**
+ * 
+ * @returns les nombres choisis dans une liste
+ */
+function retrieveNum(){
+    choosen = [] ;
+    count = 0;
+    for ([key, val] of Object.entries(number_target)) {
+        if (document.getElementById(key + "_check").checked){
+            choosen[count] = key;
+            count++ ;
+        }
+    }    
+    console.log("choosen num", choosen);
+    return choosen;
+}
+
+/**
+ * 
+ * @returns les cas choisis dans une liste
+ */
+function retrieveCases(){
+    choosen = [] ;
+    count = 0;
+    for ([key, val] of Object.entries(decl_target)) {
+        if (document.getElementById(key + "_check").checked){
+            choosen[count] = key;
+            count++ ;
+        }
+    }    
+    console.log("choosen cases", choosen);
+    return choosen;
+}
+/**
+ * 
+ *  @returns l'abbreviation du genre choisi pour la question
+ *  si c'est un adjectif seul
+ */
+function getGenderForAdjOnly(){
+    caseIdx= Math.floor(Math.random()*adj_gender_list.length);
+    abrev = adj_gender_list[caseIdx];
+    console.log("adj gender =",abrev);
+    return abrev;
+}
+/**
+ * 
+ *  @returns l'abbreviation du nombre choisi pour la question
+ */
+function getNumQuestion(){
+    caseIdx= Math.floor(Math.random()*choosenNum.length);
+    abrev = choosenNum[caseIdx];
+    console.log("num abrev =",abrev);
+    return abrev;
+}/**
+ * 
+ *  @returns l'abbreviation du cas choisi pour la question
+ */
+function getCaseQuestion(){
+    caseIdx= Math.floor(Math.random()*choosenCases.length);
+    console.log("choosen case", choosenCases)
+    abrev = choosenCases[caseIdx];
+    console.log("case abrev =",abrev);
+    return abrev;
+}
+async function startDrill(){
+    // first check whether all params  have at least one check
+    if (paramOK()) {
+        choosenCases = retrieveCases();
+        choosenPos = retrievePos();
+        choosenNum = retrieveNum();
+        caseAbrev = getCaseQuestion();
+        numAbrev = getNumQuestion();
+        genderAbrev = getGenderForAdjOnly();
+        const fs = require("fs")
+        const sqlite = require("aa-sqlite")
+        await sqlite.open('cregr_db.db');
+        let caseText = document.getElementById("case");
+        //caseText.innerHTML = await getCase(sqlite)
+    }
+}
+var pos_target ={
+    sub : true,
+    adj :false,
+    pron : false
+};
+
+
+var decl_target = {
+    nom : true,
+    gen : true,
+    acc : true,
+    dat : true,
+    inst : true,
+    prep : true
+};
+var number_target = {
+    sing : true,
+    plur : true
+}
+var adj_gender_list = ["masc", "fem", "neut","plu"]
+var freq_target = "most";
+var choosenCases = [];
+var choosenPos = [];
+var choosenNum = [];
