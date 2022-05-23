10070dec6c0c7f4b9abcfb5aa8489d3ceb7ce04b
Alain Bertrand <alainbe@free.fr>
2022-05-23 22:30:46 +0200

    progrès vers la formulation de la question et de la réponse

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById("start").addEventListener("click", startDrill);
+document.getElementById("start").addEventListener("click", initDrill);
 document.getElementById("decl").addEventListener("click", chooseDecl);
 document.getElementById("conj").addEventListener("click", chooseConj);
 document.getElementById("pos_button").addEventListener("click", togglePos);
@@ -11,6 +11,17 @@ document.getElementById("freq_button").addEventListener("click", toggleFreq);
   * GUI update functions
   * 
   * ****************************************/
+
+ /**
+  * 
+  * shows the question div
+  * 
+  */
+function showQuestion(){
+     elem =document.getElementById("decl_quizz");
+     elem.style.display ="block";
+     elem.innerHTML = decl_question['clearQuestion'];
+ }
 function toggleFreq(){
     let myDiv = document.getElementById("freq");
     if (myDiv.style.display == "block"){
@@ -102,7 +113,56 @@ async function getCase(db){
   *  drill  controlling functions
   * 
   * ****************************************/
-function paramOK() {
+
+/**
+ * 
+ * @returns a substantive choosen in the correct frequency range
+ */
+async function chooseSubstantive() {
+    
+    const fs = require("fs")
+    const sqlite = require("aa-sqlite")
+    await sqlite.open('cregr_db.db');
+    // select all the word id whose whose sub_id is < to limit
+    req ="SELECT id FROM substantives WHERE sub_id < "+ sub_freq_indices[freq_target];
+    console.log(req);
+    r = await sqlite.all(req, [])
+    let vocaId = []
+    r.forEach(function(row) {
+        vocaId.push(row.id);
+        //console.log("Read:", row.id, row.sub_id, row.nominative_singular)    
+    });
+    console.log("nombre de noms ;",vocaId.length )
+    return "";
+
+}
+
+ /**
+  * 
+  * 
+  * @returns rien mais met à jour la variable globale freq_target
+  */
+function readFrequency(value){
+    if (document.getElementById(value+"_rad").checked){
+        freq_target= value ;
+        console.log ("freq target", freq_target);
+    }
+}
+ /**
+  * 
+  *  @returns rien mais appelle la fonction readFrequency
+  * 
+
+  */
+ function updateFrequency(){
+     but = ['most','med','all'];
+     but.forEach(readFrequency);
+ }
+
+
+
+
+ function paramOK() {
     // check pos
     result= true;
     posOK = false;
@@ -151,20 +211,21 @@ function paramOK() {
 
 /**
  * 
- * @returns les types grammaticaux choisis dans une liste
+ * @returns rien mais modifie les valeur de la variable globale pos_target
  */
 function retrievePos(){ 
-    choosen = [] ;
-    count = 0;
+    //choosen = {sub : false, adj: false, pron : false};
     for ([key, val] of Object.entries(pos_target)) {
-        //console.log("chain = ", chain);
+        chain = key + "_check";
+        console.log("chain = ", chain);
         if (document.getElementById(key + "_check").checked){
-            choosen[count] = key;
-            count++ ;
+            pos_target[key] = true;
+            //console.log(pos_target[key]);
+        } else{
+            pos_target[key] = false;
         }
     }
-    console.log("choosen ps", choosen);
-    return choosen
+    console.log("choosen pos", pos_target);
 }
 /**
  * 
@@ -193,11 +254,22 @@ function retrieveCases(){
     for ([key, val] of Object.entries(decl_target)) {
         if (document.getElementById(key + "_check").checked){
             choosen[count] = key;
-            count++ ;
-        }
+            count++;
+        } 
     }    
     console.log("choosen cases", choosen);
     return choosen;
+}
+/**
+ * 
+ * @returns random choice of animate or inanimate 
+ */
+function getAnimQuestion(){
+    animS = 'inam';
+    if (Math.random() >0.5){
+        animS ='anim';
+    }
+    return animS;
 }
 /**
  * 
@@ -215,6 +287,7 @@ function getGenderForAdjOnly(){
  *  @returns l'abbreviation du nombre choisi pour la question
  */
 function getNumQuestion(){
+    choosenNum = retrieveNum();
     caseIdx= Math.floor(Math.random()*choosenNum.length);
     abrev = choosenNum[caseIdx];
     console.log("num abrev =",abrev);
@@ -224,34 +297,73 @@ function getNumQuestion(){
  *  @returns l'abbreviation du cas choisi pour la question
  */
 function getCaseQuestion(){
+    choosenCases = retrieveCases();
     caseIdx= Math.floor(Math.random()*choosenCases.length);
+    console.log("choosen case lenght ", choosenCases.length);
     console.log("choosen case", choosenCases)
+    console.log("caseIdx  ", caseIdx)
     abrev = choosenCases[caseIdx];
     console.log("case abrev =",abrev);
     return abrev;
 }
-async function startDrill(){
+ function initDrill(){
     // first check whether all params  have at least one check
     if (paramOK()) {
-        choosenCases = retrieveCases();
+        //choosenCases = retrieveCases();
         choosenPos = retrievePos();
-        choosenNum = retrieveNum();
+        //choosenNum = retrieveNum();
         caseAbrev = getCaseQuestion();
         numAbrev = getNumQuestion();
         genderAbrev = getGenderForAdjOnly();
-        const fs = require("fs")
-        const sqlite = require("aa-sqlite")
-        await sqlite.open('cregr_db.db');
-        let caseText = document.getElementById("case");
-        //caseText.innerHTML = await getCase(sqlite)
+        //decl_question[cas]= caseAbrev;
+        decl_question[num] = numAbrev;
+        runDrill();
     }
 }
+async function runDrill(){
+    // start to query db
+    const fs = require("fs")
+    const sqlite = require("aa-sqlite")
+    await sqlite.open('cregr_db.db');
+    // find  the appelation of the case
+    let req = "SELECT french FROM cas_usuels WHERE abrev ='" + caseAbrev + "'"
+    console.log('req : ', req)
+
+    r = await sqlite.get(req)
+    gramQuestion = r.french;
+    req = "SELECT french FROM vocabulaire_grammatical WHERE abrev = '" + numAbrev + "'";
+    r = await sqlite.get(req);
+    gramQuestion = gramQuestion + " " + r.french;
+    gendQuestion = "";
+    console.log("choosen pos", pos_target);
+    console.log("valeur de sub", pos_target["sub"]);
+    if (!pos_target["sub"] && caseAbrev == 'acc') {
+        // choix de animé
+        animQ = getAnimQuestion();
+        decl_question['anim'] = animQ;
+        console.log("anim ", animQ);
+        req = " SELECT french FROM vocabulaire_grammatical WHERE abrev = '" + animQ + "'";
+        r = await sqlite.get(req);
+        gramQuestion = gramQuestion + " " + r.french;
+    }
+    
+    decl_question["clearQuestion"] = gramQuestion;
+    updateFrequency();
+    //chose the words
+    if (pos_target['sub']){
+        decl_question['sub'] = chooseSubstantive();
+    }
+    showQuestion();
+    //caseText.innerHTML = await getCase(sqlite)
+}
+
 var pos_target ={
     sub : true,
     adj :false,
     pron : false
 };
 
+var decl_cases =['nom','gen','acc','dat','inst','prep'];
 
 var decl_target = {
     nom : true,
@@ -268,5 +380,26 @@ var number_target = {
 var adj_gender_list = ["masc", "fem", "neut","plu"]
 var freq_target = "most";
 var choosenCases = [];
-var choosenPos = [];
+var choosenPos = {sub : false, adj: false, pron : false};
 var choosenNum = [];
+
+var decl_question = {
+ case : "",
+ num : "",
+ isAnim :"",
+ clearQuestion : "",
+ sub : "",
+ adj_: "",
+ pron :"",
+ answer :""
+};
+
+const sub_freq_indices ={
+    most : "434",
+    med : "1481",
+    all : "10000"
+};
+const adj_freq_indices = {
+    most: 883,
+    med: 3753
+}
